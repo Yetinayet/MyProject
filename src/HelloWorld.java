382d1861da82be3825b47f53d1163f3228797384
Yetinayet <danyetm1994@gmail.com>
2020-07-11 20:41:42 -0500

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld {// class body start
+	
+	// .java is the extension of class
+	//src means source
+	//package name start with small letter
+	
+	// class syntax : AccessModifier class className{  }
+
+	// class body
+	
+	// main method: driver method/ Executable method
+	public static void main (String args[]) {// method body start
+		// Always JVM(Java Virtual machine) only execute main method
+		
+		// To print anything in console
+		System.out.println("Hello Tasmie"); // to close any statement we need to use semi-colon ;
+		
+		// inside double quote what ever you write ,its known as String.
+		System.out.println  ("Hello !! World");
+		
+		System.out.println("Welcome to QA Automation Engineering class");
+		
+		
+		
+	} // method body close
+	
+	
+} // class body end
---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class HelloWorld {
+
+    // psvm for main method
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World");
+    }
+
+
+
+
+}
